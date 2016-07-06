9f6674e7d75149d906e2299fb539a6f2b4ca03c2
Guergana <pestanias@yahoo.com>
2016-07-05 21:10:05 -0500

    audio thing

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static('public'));
+
 io.on('connection', function(socket){
   console.log('a user connected');
    socket.on('disconnect', function(){
