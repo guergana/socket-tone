e8d1e51abdd480677b491fdb6c6839a94c02bd66
Guergana <pestanias@yahoo.com>
2016-07-05 21:29:22 -0500

    added express

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-//res.SendFile("public/audio/1.mp3")
+res.SendFile("public/audio/1.mp3")
 
 
 io.on('connection', function(socket){
