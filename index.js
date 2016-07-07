2ebb4050d10905f46e8b5b7f1bcc960fc1782765
Guergana <pestanias@yahoo.com>
2016-07-07 11:44:47 -0500

    added audio folder

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/audio'));
 
 var port = process.env.PORT || 3000;
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -19,13 +19,15 @@ app.get('/audio/2.mp3', function(req, res){
 });
 
 io.on('connection', function(socket){
+	
   console.log('a user connected');
    socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+	 
+    console.log( 'message: ' + msg);
     io.emit('chat message', msg);
   });
 });
